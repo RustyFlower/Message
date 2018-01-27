45169e6a58a1fda005cfe7d2abb12b9d284820b6
Ren Matsushita <ren1725m@gmail.com>
2018-01-27 19:49:45 +0900

    メッセージ機能の実装

--- Message/ViewController.swift
+++ Message/ViewController.swift
@@ -7,14 +7,82 @@
 //
 
 import UIKit
+import Firebase
+import JSQMessagesViewController
 
-class ViewController: UIViewController {
+class ViewController: JSQMessagesViewController {
+    
+    var messages: [JSQMessage]?
+    
+    var incomingBubble: JSQMessagesBubbleImage!
+    var outgoingBubble: JSQMessagesBubbleImage!
+    var incomingAvatar: JSQMessagesAvatarImage!
+    var outgoingAvatar: JSQMessagesAvatarImage!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupFirebase()
+        setupChatUI()
+        
+        self.messages = []
     }
-
+    
+    func setupFirebase() {
+        
+        
+    }
+    
+    func setupChatUI() {
+        inputToolbar!.contentView!.leftBarButtonItem = nil
+        automaticallyScrollsToMostRecentMessage = true
+        
+        self.senderId = "user1"
+        self.incomingAvatar = JSQMessagesAvatarImageFactory.avatarImage(with: UIImage(named: "icon_default")!, diameter: 64)
+        self.outgoingAvatar = JSQMessagesAvatarImageFactory.avatarImage(with: UIImage(named: "icon_default")!, diameter: 64)
+        
+        let bubbleFactory = JSQMessagesBubbleImageFactory()
+        self.incomingBubble = bubbleFactory?.incomingMessagesBubbleImage(
+            with: UIColor.jsq_messageBubbleLightGray())
+        self.outgoingBubble = bubbleFactory?.outgoingMessagesBubbleImage(
+            with: UIColor.jsq_messageBubbleGreen())
+    }
+    
+    //メッセージの送信
+    override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+        self.finishSendingMessage(animated: true)
+        sendTextToDb(text: text)
+    }
+    
+    func sendTextToDb(text: String) {
+        //データベースへの送信（後述）
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return self.messages?[indexPath.item]
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = self.messages?[indexPath.item]
+        if message?.senderId == self.senderId {
+            return self.outgoingBubble
+        }
+        return self.incomingBubble
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        let message = self.messages?[indexPath.item]
+        if message?.senderId == self.senderId {
+            return self.outgoingAvatar
+        }
+        return self.incomingAvatar
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (self.messages?.count)!
+    }
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +90,3 @@ class ViewController: UIViewController {
 
 
 }
-
